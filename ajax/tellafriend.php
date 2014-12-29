fa9e7fc23dd5c0eb1c5cebb8cfb034eb3de090d7
JTinPlano <jt@mystagingbox.com>
2014-12-28 20:42:31 -0600

    Corrected head sjavacript and css files

--- ajax/tellafriend.php
+++ ajax/tellafriend.php
@@ -18,34 +18,41 @@ get_globals();
 //$to=$toemail;
 //$from="noreply@mystagingbox.com";
 //$subject="Subject";
-	$content = "
-	<!DOCTYPE HTML>
-	<html dir='LTR' lang='en-us'>
-	<head>
-	<title>MyStagingBox.com Registration</title>
-		<link type='text/css' href='$_SERVER[HTTP_HOST]/css/jquery-ui-1.8.14.dark-hive.css' rel='stylesheet' />
-		<link type='text/css' href='$_SERVER[HTTP_HOST]/css/prod.css' rel='stylesheet' />
-		<script type='text/javascript' src='http://code.jquery.com/jquery-1.7.2.min.js'></script>
-		<script type='text/javascript' src='$_SERVER[HTTP_HOST]/js/jquery-ui-1.8.14.dark-hive.min.js'></script>
-		<link type='text/css' href='$_SERVER[HTTP_HOST]/css/tooltip.css' rel='stylesheet' />
-		<script type='text/javascript' src='$_SERVER[HTTP_HOST]/js/hrsb.js'></script>
-		<script type='text/javascript' src='$_SERVER[HTTP_HOST]/js/jquery.cookies.2.2.0.js'></script>
-		<script type='text/javascript' src='$_SERVER[HTTP_HOST]/js/jquery.blockUI.js'></script>
-	<script type='text/javascript' src='$_SERVER[HTTP_HOST]/js/charCount.js'></script>
-	<script type='text/javascript' src='$_SERVER[HTTP_HOST]/js/jTip.js'></script>
-	</head>
-	<body>
-	<header>
-	<logo><img alt='MyStagingBox.com' src='$_SERVER[HTTP_HOST]/images/hrsb-logo.gif' border='0'></logo>
-	</header>
+	$content = '
+<html dir="LTR" lang="en-us">
+<head>
+	<link type="text/css" href="http://'.$_SERVER["SERVER_NAME"].'/css/jquery-ui-1.8.14.dark-hive.css" rel="stylesheet" />
+	<link type="text/css" href="http://'.$_SERVER["SERVER_NAME"].'/css/prod.css" rel="stylesheet" />
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="http://'.$_SERVER["SERVER_NAME"].'/js/jquery-ui-1.8.14.dark-hive.min.js"></script>
+	<link type="text/css" href="http://'.$_SERVER["SERVER_NAME"].'/css/tooltip.css" rel="stylesheet" />
+	<script type="text/javascript" src="http://'.$_SERVER["SERVER_NAME"].'/js/hrsb.js"></script>
+	<script type="text/javascript" src="http://'.$_SERVER["SERVER_NAME"].'/js/jquery.cookies.2.2.0.js"></script>
+<title>MyStagingBox.com Registration Confirmation</title>
+</head>
+<body>
+<header>
+<dashboard>
+</dashboard>
+<logo>
+<img alt="MyStagingBox.com" src="http://'.$_SERVER["SERVER_NAME"].'/images/msb-logo7.gif" border="0">
+</logo>
+</header>
 	<table>
 	<tr><td>
-		$subject.$message<br /><br />.
+		'.$subject.'<br /><br />'.$message.'<br /><br />
 	</td></tr>
+	<tr><td>
+		<form action = "http://'.$_SERVER["SERVER_NAME"].'">
+		<input type="submit" value="Go to MyStagingBox.com" class="ui-button ui-widget ui-state-default ui-corner-all">
+		</form>
+	</td></tr>
+
 	</table>
 	</body>
-	</html>";
-
+	</html>';
+// echo "content:<br />".$content;
+// exit();
 	// Always set content-type when sending HTML email
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
@@ -68,7 +75,7 @@ $headers .= 'CC: '.$from.'' . "\r\n";
 //echo "message ".$content."<br />";
 //echo "header: ".$headers."<br />";
 //	exit();
-if (mail($to, $subject, $message, $headers))
+if (mail($to, $subject, $content, $headers))
 {
 	$return['error'] = false;
 	$return['msg'] = 'Your email has been sent, and a copy has been sent to you.  Click "Cancel" below to continue.';
