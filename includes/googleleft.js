cf8de07f0fb8f7835a5f67c70fe7b3f23a4407f7
JTinPlano <jt@mystagingbox.com>
2015-01-11 14:23:36 -0600

    Added Google Adsense files

---
+++ includes/googleleft.js
@@ -0,0 +1,20 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- msb = 120X240 - 1/11/15 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:120px;height:240px"
+     data-ad-client="ca-pub-0084848109834460"
+     data-ad-slot="5700236053"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+Bottom ad
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- MSB - 728x90 - 1/11/15 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-0084848109834460"
+     data-ad-slot="7176969253"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ includes/googleright.js
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- msb = 120X240 - 1/11/15 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:120px;height:240px"
+     data-ad-client="ca-pub-0084848109834460"
+     data-ad-slot="5700236053"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
