5dd28d9bd50e13b7af818dd077d2eab4fafbfcbf
JTinPlano <jt@mystagingbox.com>
2015-01-14 10:33:08 -0600

    Deleted extra Google Adsense code

--- includes/googleleft.js
+++ includes/googleleft.js
@@ -7,14 +7,3 @@
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-
-Bottom ad
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- MSB - 728x90 - 1/11/15 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-0084848109834460"
-     data-ad-slot="7176969253"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
